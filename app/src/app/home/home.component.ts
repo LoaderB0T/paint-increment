76ee3b1cf784d24fc29caeeb3d38f63ed5e99211
Janik Schumacher <rndmb0t@gmail.com>
2021-07-30 21:59:07 +0200

    style revamp + homepage

--- app/src/app/home/home.component.ts
+++ app/src/app/home/home.component.ts
@@ -1,14 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
+  private readonly _router: Router;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(router: Router) {
+    this._router = router;
   }
 
+  public navigateToNewLobby() {
+    this._router.navigate(['new']);
+  }
 }
