8e0c9531a590bd24c09c516d87c0f7f307571c45
Janik Schumacher <rndmb0t@gmail.com>
2021-08-22 19:55:38 +0200

    fixed increasing subscriptions

--- app/src/app/services/ws.service.ts
+++ app/src/app/services/ws.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { io, Socket } from 'socket.io-client';
 import { environment } from '../../environments/environment';
 import { ExtractPayload, WsCommunication, WsReceiveMessage, WsSendMessage } from '../models/ws-event-types.model';
@@ -11,6 +11,7 @@ import { IdService } from './id.service';
 export class WsService {
   private readonly _socket: Socket;
   private readonly _idService: IdService;
+  private readonly _listens: { [key: string]: any } = {};
 
   constructor(idService: IdService) {
     this._idService = idService;
@@ -21,11 +22,7 @@ export class WsService {
     });
   }
 
-  public init() {
-    this._socket.onAny((name: string, data: any) => {
-      console.log(`WS ${name}`, data);
-    });
-  }
+  public init() {}
 
   public send<T extends WsSendMessage>(method: T, payload: ExtractPayload<WsCommunication, T>): void {
     payload.uid = this._idService.id;
@@ -34,11 +31,17 @@ export class WsService {
   }
 
   public listen<T extends WsReceiveMessage>(method: T): Observable<ExtractPayload<WsCommunication, T>> {
-    return new Observable<ExtractPayload<WsCommunication, T>>(observer => {
-      this._socket.on(method as string, (data: ExtractPayload<WsCommunication, T>) => {
-        console.log(`WS listened: ${method}`, data);
-        observer.next(data);
-      });
+    const existingListener = this._listens[method];
+    if (existingListener) {
+      return existingListener;
+    }
+    const subject = new Subject<ExtractPayload<WsCommunication, T>>();
+    this._socket.on(method as string, (data: ExtractPayload<WsCommunication, T>) => {
+      console.log(`WS listened: ${method}`, data);
+      subject.next(data);
     });
+    const obs = subject.asObservable();
+    this._listens[method] = obs;
+    return obs;
   }
 }
