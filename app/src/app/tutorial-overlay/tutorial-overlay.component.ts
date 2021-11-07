0831400ff7b7af2fd2c3dd1331574d9a7a050def
Janik Schumacher <rndmb0t@gmail.com>
2021-11-07 19:42:50 +0100

    added control tutorial

--- app/src/app/tutorial-overlay/tutorial-overlay.component.ts
+++ app/src/app/tutorial-overlay/tutorial-overlay.component.ts
@@ -14,6 +14,7 @@ export class TutorialOverlayComponent {
   @Input() lockedBySomeone: boolean = false;
   @Input() lockedByMe: boolean = false;
   @Input() pendingConfirm: boolean = false;
+  @Input() showControls: boolean = false;
 
   constructor() {}
 
