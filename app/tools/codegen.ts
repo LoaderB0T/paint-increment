f4c97043d2105ae77ca982a63078d9426b379eba
Janik Schumacher <rndmb0t@gmail.com>
2024-12-23 03:30:43 +0100

    .

--- app/tools/codegen.ts
+++ app/tools/codegen.ts
@@ -1,17 +1,19 @@
+import { existsSync } from 'node:fs';
+
 import { OpenApiGenerator } from '@goast/core';
-import {
-  TypeScriptModelsGenerator,
-  TypeScriptAngularServicesGenerator,
-} from '@goast/typescript';
+import { TypeScriptModelsGenerator, TypeScriptAngularServicesGenerator } from '@goast/typescript';
 
 async function main() {
   const generator = new OpenApiGenerator({ outputDir: 'src/shared/api/.api' })
-    // Add a generator using a class
     .useType(TypeScriptModelsGenerator)
     .useType(TypeScriptAngularServicesGenerator);
 
-  // Generate for one of more OpenAPI specification files
-  await generator.parseAndGenerate('../api/swagger.json');
+  const filePathLocal = '../api/swagger.json';
+  const filePathDocker = 'api/swagger.json';
+
+  const filePath = existsSync(filePathLocal) ? filePathLocal : filePathDocker;
+
+  await generator.parseAndGenerate(filePath);
 }
 
 main();
