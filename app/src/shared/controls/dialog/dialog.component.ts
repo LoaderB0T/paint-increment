c6813c1fb62cfe14a7a819d6ad27ff3b3ae039ae
Janik Schumacher <rndmb0t@gmail.com>
2024-12-30 00:38:15 +0100

    tape

--- app/src/shared/controls/dialog/dialog.component.ts
+++ app/src/shared/controls/dialog/dialog.component.ts
@@ -9,4 +9,5 @@ import { ChangeDetectionStrategy, Component, viewChild, ViewContainerRef } from 
 })
 export class DialogComponent {
   public readonly container = viewChild.required('container', { read: ViewContainerRef });
+  protected readonly randomTapeSrc = `/tape/tape${Math.floor(Math.random() * 10) + 1}.png`;
 }
